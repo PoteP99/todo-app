abf594e0e1ce0d0ffa061a7c50dcd3eaec656a27
Pote Prathumma <poteprathumma@Potes-MacBook-Pro.local>
2025-07-23 15:48:49 +0700

    addTodo function. Added drag indicator. Need work on index system

---
+++ script.js
@@ -0,0 +1,70 @@
+const todoInput = document.getElementById("todo-input");
+const addButton = document.querySelector("#add-button");
+const todoList = document.querySelector("#todo-list");
+
+addButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  addTodo();
+});
+
+let allTodos = [];
+
+function addTodo(todo) {
+  const todoText = todoInput.value.trim();
+  const todoObject = {
+    id: allTodos.length + 1,
+    text: todoText,
+    completed: false,
+  };
+  allTodos.push(todoObject);
+  todoIndex = "todo-" + allTodos.length;
+  const todoLI = document.createElement("li");
+  todoLI.className = "todo";
+  todoLI.innerHTML = `
+      <input type="checkbox" id="${todoIndex}" />
+      <label for="${todoIndex}" class="drag-indicator">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M360-160q-33 0-56.5-23.5T280-240q0-33 23.5-56.5T360-320q33 0 56.5 23.5T440-240q0 33-23.5 56.5T360-160Zm240 0q-33 0-56.5-23.5T520-240q0-33 23.5-56.5T600-320q33 0 56.5 23.5T680-240q0 33-23.5 56.5T600-160ZM360-400q-33 0-56.5-23.5T280-480q0-33 23.5-56.5T360-560q33 0 56.5 23.5T440-480q0 33-23.5 56.5T360-400Zm240 0q-33 0-56.5-23.5T520-480q0-33 23.5-56.5T600-560q33 0 56.5 23.5T680-480q0 33-23.5 56.5T600-400ZM360-640q-33 0-56.5-23.5T280-720q0-33 23.5-56.5T360-800q33 0 56.5 23.5T440-720q0 33-23.5 56.5T360-640Zm240 0q-33 0-56.5-23.5T520-720q0-33 23.5-56.5T600-800q33 0 56.5 23.5T680-720q0 33-23.5 56.5T600-640Z"/></svg>
+      </label>
+      <label for="${todoIndex}" class="custom-checkbox">
+        <svg
+          fill="var(--card-bg)"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#1f1f1f"
+        >
+          <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+        </svg>
+      </label>
+      <label for="${todoIndex}" class="todo-text">${todoText}</label>
+      <button for="${todoIndex}" class="schedule-button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#1f1f1f"
+        >
+          <path
+            d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"
+          />
+        </svg>
+      </button>
+      <button class="delete-button">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#1f1f1f"
+        >
+          <path
+            d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+          />
+        </svg>
+      </button>
+  `;
+  todoList.append(todoLI);
+  todoInput.value = "";
+}
